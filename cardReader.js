62a6deded49f8a29a5f6bbbc6a3c9a6519841d2d
Joaquín Ormaechea <joaquinormaechea@gmail.com>
2019-10-16 16:21:09 -0300

    Eslint added and errors code style fixed

--- cardReader.js
+++ cardReader.js
@@ -1,143 +1,139 @@
-var ref = require('ref');
+'use strict';
 
-var error = require('./error');
-var types = require('./types');
-var libModule =  require('./libModule');
+/* eslint-disable-next-line */
+const ref = require('ref');
+
+const error = require('./error');
+const types = require('./types');
+const libModule = require('./libModule');
 
 /**
  * Get connected device list
  */
-exports.selectDevice = function () {
-    return new Promise(function(resolve, reject) {
+exports.selectDevice = function() {
+	return new Promise((resolve, reject) => {
 
-        // char ReaderBuffer[1024]
-        var readerBuffer = Buffer.alloc(1024, 0);
-        // DWORD *ReaderBufferlen
-        var readerBufferlen = ref.alloc(types.dword);
+		// char ReaderBuffer[1024]
+		const readerBuffer = Buffer.alloc(1024, 0);
+		// DWORD *ReaderBufferlen
+		const readerBufferlen = ref.alloc(types.dword);
 
-        var ret = libModule.MT188_SelectDevice(readerBuffer, readerBufferlen);
+		const ret = libModule.MT188_SelectDevice(readerBuffer, readerBufferlen);
 
-        if(ret != 0) {
-            reject(error(ret));
-        } else {
-            var devices = [];
-            var offset = 0;
-            var length = ref.deref(readerBufferlen);
+		if(ret !== 0)
+			return reject(error(ret));
+		const devices = [];
+		let offset = 0;
+		const length = ref.deref(readerBufferlen);
 
-            while(offset < length - 1) { // list trailing zero
-                var device = ref.readCString(readerBuffer, offset);
-                devices.push(device);
-                offset += device.length + 1; // string trailing zero
-            }
+		while(offset < length - 1) { // list trailing zero
+			const device = ref.readCString(readerBuffer, offset);
+			devices.push(device);
+			offset += device.length + 1; // string trailing zero
+		}
 
-            resolve(devices);
-        }
-    });
-}
+		resolve(devices);
+	});
+};
 
 /**
  * Can communicate with device only when connection succeeds.
  * @param {string} readerName  name of device waiting for connection
  */
-exports.open = function (readerName) {
-    return new Promise(function(resolve, reject) {
+exports.open = function(readerName) {
+	return new Promise((resolve, reject) => {
 
-        var readerNameCStr = ref.allocCString(readerName);
-        var ret = libModule.MT188_Open(readerNameCStr);
+		const readerNameCStr = ref.allocCString(readerName);
+		const ret = libModule.MT188_Open(readerNameCStr);
 
-        if(ret != 0) {
-            reject(error(ret));
-        } else {
-            resolve();
-        }
-    });
-}
+		if(ret !== 0)
+			return reject(error(ret));
+
+		return resolve();
+	});
+};
 
 /**
  * Disconnection for device
  */
-exports.close = function () {
-    return new Promise(function(resolve, reject) {
-        var ret = libModule.MT188_Close();
+exports.close = function() {
+	return new Promise((resolve, reject) => {
+		const ret = libModule.MT188_Close();
 
-        if(ret != 0) {
-            reject(error(ret));
-        } else {
-            resolve();
-        }
-    });
-}
+		if(ret !== 0)
+			return reject(error(ret));
+
+		resolve();
+	});
+};
 
 /**
  * Magnetic card tracks
  */
 exports.MagneticCard = {
-    Track1: 0x31,
-    Track2: 0x32,
-    Track3: 0x33
-}
+	Track1: 0x31,
+	Track2: 0x32,
+	Track3: 0x33
+};
 
 /**
  * Read data from specified track
  * @param {byte} track Track option: MagneticCard.Track1, MagneticCard.Track2, MagneticCard.Track3
  */
 exports.readMagneticCard = function(track) {
-    return new Promise(function(resolve, reject) {
+	return new Promise((resolve, reject) => {
 
-        //BYTE Data[1024] = {0};
-        var data = Buffer.alloc(1024, 0);
-        //int Len = 0;
-        var lenIntPtr = ref.alloc(ref.types.int, 0);
+		// BYTE Data[1024] = {0};
+		const data = Buffer.alloc(1024, 0);
+		// int Len = 0;
+		const lenIntPtr = ref.alloc(ref.types.int, 0);
 
-        var ret = libModule.MT188_ReadMagneticCard(track, data, lenIntPtr);
+		const ret = libModule.MT188_ReadMagneticCard(track, data, lenIntPtr);
 
-        if(ret != 0) {
-            reject(error(ret));
-        } else {
-            var len = ref.deref(lenIntPtr);
-            if(len == 0x02) {
-                var firstByte = ref.get(data, 0, ref.types.byte);
-                if (firstByte == 0xE1 || firstByte == 0xE2 || firstByte == 0xE3) {
-                    reject("Failure");
-                }
-            } else {
-                var str = ref.readCString(data, 0);
-                resolve(str);
-            }
-        }
-    });
-}
+		if(ret !== 0)
+			return reject(error(ret));
+
+		const len = ref.deref(lenIntPtr);
+		if(len === 0x02) {
+			const firstByte = ref.get(data, 0, ref.types.byte);
+			if(firstByte === 0xE1 || firstByte === 0xE2 || firstByte === 0xE3)
+				reject(new Error(`Magnetic card failure: ${data}`));
+		} else {
+			const str = ref.readCString(data, 0);
+			resolve(str);
+		}
+	});
+};
 
 /**
  * Clear buffer area of magnetic card
  */
 exports.clearMagneticCard = function() {
-    return new Promise(function(resolve, reject) {
-        var ret = libModule.MT188_ClearMagneticCard();
+	return new Promise((resolve, reject) => {
+		const ret = libModule.MT188_ClearMagneticCard();
 
-        if(ret != 0) {
-            reject(error(ret));
-        } else {
-            resolve();
-        }
-    });
-}
+		if(ret !== 0)
+			return reject(error(ret));
+
+		resolve();
+	});
+};
 
 const CardType = {
-    NoCardInside: 0x30,
-    ContactIC: 0x31,
-    ContactlessTypeA: 0x32,
-    ContactlessTypeB: 0x33,
-    ContactlessM1: 0x34,
-    Unidentified: 0x3f
-}
+	NoCardInside: 0x30,
+	ContactIC: 0x31,
+	ContactlessTypeA: 0x32,
+	ContactlessTypeB: 0x33,
+	ContactlessM1: 0x34,
+	Unidentified: 0x3f
+};
 
 exports.CardType = CardType;
 
 const MagneticCardType = {
-    NoReadableMagneticCardInfo: 0x30,
-    HasReadableMagneticCardInfo: 0x31
-}
+	NoReadableMagneticCardInfo: 0x30,
+	HasReadableMagneticCardInfo: 0x31
+};
 
 exports.MagneticCardType = MagneticCardType;
 
@@ -145,36 +141,35 @@ exports.MagneticCardType = MagneticCardType;
  * Get card type inside card reader
  */
 exports.getCardType = function() {
-    return new Promise(function(resolve, reject) {
-        // BYTE *Type
-        var typeBytePtr = ref.alloc(ref.types.byte, 0);
+	return new Promise((resolve, reject) => {
+		// BYTE *Type
+		const typeBytePtr = ref.alloc(ref.types.byte, 0);
 
-        // BYTE *Magnetic
-        var magneticBytePtr = ref.alloc(ref.types.byte, 0);
+		// BYTE *Magnetic
+		const magneticBytePtr = ref.alloc(ref.types.byte, 0);
 
-        var ret = libModule.MT188_GetCardType(typeBytePtr, magneticBytePtr);
+		const ret = libModule.MT188_GetCardType(typeBytePtr, magneticBytePtr);
 
-        if(ret != 0) {
-            reject(error(ret));
-        } else {
-            var type = ref.deref(typeBytePtr);
-            var magnetic = ref.deref(magneticBytePtr);
+		if(ret !== 0)
+			return reject(error(ret));
 
-            resolve({
-                type: type,
-                magnetic: magnetic
-            });
-        }
-    });
-}
+		const type = ref.deref(typeBytePtr);
+		const magnetic = ref.deref(magneticBytePtr);
+
+		resolve({
+			type,
+			magnetic
+		});
+	});
+};
 
 const SIMNumber = {
-    ContactCPUCard: 0,
-    SIM1: 1,
-    SIM2: 2,
-    SIM3: 3,
-    SIM4: 4
-}
+	ContactCPUCard: 0,
+	SIM1: 1,
+	SIM2: 2,
+	SIM3: 3,
+	SIM4: 4
+};
 
 exports.SIMNumber = SIMNumber;
 
@@ -182,129 +177,123 @@ exports.SIMNumber = SIMNumber;
  * SIM card activation
  * @param {SIMNumber} SIMNo Card slot number.
  */
-exports.SIMPowerOn = function (SIMNo) {
-    return new Promise(function(resolve, reject) {
-        // BYTE ATR[1024]
-        var ATRBuffer = Buffer.alloc(1024, 0);
+exports.SIMPowerOn = function(SIMNo) {
+	return new Promise((resolve, reject) => {
+		// BYTE ATR[1024]
+		const ATRBuffer = Buffer.alloc(1024, 0);
 
-        // DWORD *ATRLen
-        var ATRLenDWordPtr = ref.alloc(types.dword, 0);
+		// DWORD *ATRLen
+		const ATRLenDWordPtr = ref.alloc(types.dword, 0);
 
-        var ret = libModule.MT188_SIM_PowerOn(SIMNo, ATRBuffer, ATRLenDWordPtr);
+		const ret = libModule.MT188_SIM_PowerOn(SIMNo, ATRBuffer, ATRLenDWordPtr);
 
-        if(ret != 0) {
-            reject(error(ret));
-        } else {
-            var len = ref.deref(ATRLenDWordPtr) * 2; // Warning! Legnth is wrong informed, or doesnt match demo app. We're getting half of the value.
-            var ATR = ATRBuffer.toString('hex').substr(0, len);
-            resolve(ATR);
-        }
-    });
-}
+		if(ret !== 0)
+			return reject(error(ret));
+
+		const len = ref.deref(ATRLenDWordPtr) * 2; // Warning! Legnth is wrong informed, or doesnt match demo app. We're getting half of the value.
+		const ATR = ATRBuffer.toString('hex').substr(0, len);
+		resolve(ATR);
+	});
+};
 
 /**
  * SIM card data transfer command
  * @param {SIMNumber} SIMNo Card slot number.
  */
-exports.SIMSendAPDU = function (SIMNo, sendData) {
-    return new Promise(function(resolve, reject) {
-        // BYTE SendData[1024]
-        var sendDataBuffer = Buffer.from(sendData, 'hex');
+exports.SIMSendAPDU = function(SIMNo, sendData) {
+	return new Promise((resolve, reject) => {
+		// BYTE SendData[1024]
+		const sendDataBuffer = Buffer.from(sendData, 'hex');
 
-        // BYTE RecvData[1024]
-        var recvDataBuffer = Buffer.alloc(1024, 0);
+		// BYTE RecvData[1024]
+		const recvDataBuffer = Buffer.alloc(1024, 0);
 
-        // DWORD *RecvDataLen
-        var recvDataLenDWordPtr = ref.alloc(types.dword, 0);
+		// DWORD *RecvDataLen
+		const recvDataLenDWordPtr = ref.alloc(types.dword, 0);
 
-        var ret = libModule.MT188_SIM_APDU(SIMNo, sendDataBuffer, sendData.length / 2, recvDataBuffer, recvDataLenDWordPtr);
+		const ret = libModule.MT188_SIM_APDU(SIMNo, sendDataBuffer, sendData.length / 2, recvDataBuffer, recvDataLenDWordPtr);
 
-        if(ret != 0) {
-            reject(error(ret));
-        } else {
-            var len = ref.deref(recvDataLenDWordPtr) * 2; // Warning! Legnth is wrong informed, or doesnt match demo app. We're getting half of the value.
-            var recvData = recvDataBuffer.toString('hex').substr(0, len);
-            resolve(recvData);
-        }
-    });
-}
+		if(ret !== 0)
+			return reject(error(ret));
+
+		const len = ref.deref(recvDataLenDWordPtr) * 2; // Warning! Legnth is wrong informed, or doesnt match demo app. We're getting half of the value.
+		const recvData = recvDataBuffer.toString('hex').substr(0, len);
+		resolve(recvData);
+	});
+};
 
 /**
  * SIM card power off
  * @param {SIMNumber} SIMNo Card slot number.
  */
-exports.SIMPowerOff = function (SIMNo) {
-    return new Promise(function(resolve, reject) {
-        var ret = libModule.MT188_SIM_PowerOff(SIMNo);
+exports.SIMPowerOff = function(SIMNo) {
+	return new Promise((resolve, reject) => {
+		const ret = libModule.MT188_SIM_PowerOff(SIMNo);
 
-        if(ret != 0) {
-            reject(error(ret));
-        } else {
-            resolve();
-        }
-    });
-}
+		if(ret !== 0)
+			return reject(error(ret));
+
+		resolve();
+	});
+};
 
 /**
  * RFA card activation
  */
-exports.RFAPowerOn = function () {
-    return new Promise(function(resolve, reject) {
-        // BYTE ATR[1024]
-        var ATRBuffer = Buffer.alloc(1024, 0);
+exports.RFAPowerOn = function() {
+	return new Promise((resolve, reject) => {
+		// BYTE ATR[1024]
+		const ATRBuffer = Buffer.alloc(1024, 0);
 
-        // DWORD *ATRLen
-        var ATRLenDWordPtr = ref.alloc(types.dword, 0);
+		// DWORD *ATRLen
+		const ATRLenDWordPtr = ref.alloc(types.dword, 0);
 
-        var ret = libModule.MT188_RFA_PowerOn(ATRBuffer, ATRLenDWordPtr);
+		const ret = libModule.MT188_RFA_PowerOn(ATRBuffer, ATRLenDWordPtr);
 
-        if(ret != 0) {
-            reject(error(ret));
-        } else {
-            var len = ref.deref(ATRLenDWordPtr) * 2;
-            var ATR = ATRBuffer.toString('hex').substr(0, len);
-            resolve(ATR);
-        }
-    });
-}
+		if(ret !== 0)
+			return reject(error(ret));
+
+		const len = ref.deref(ATRLenDWordPtr) * 2;
+		const ATR = ATRBuffer.toString('hex').substr(0, len);
+		resolve(ATR);
+	});
+};
 
 /**
  * RFA card data transfer, confirm activation succeeded before execution.
  */
-exports.RFASendAPDU = function (sendData) {
-    return new Promise(function(resolve, reject) {
-        // BYTE SendData[1024]
-        var sendDataBuffer = Buffer.from(sendData, 'hex');
+exports.RFASendAPDU = function(sendData) {
+	return new Promise((resolve, reject) => {
+		// BYTE SendData[1024]
+		const sendDataBuffer = Buffer.from(sendData, 'hex');
 
-        // BYTE RecvData[1024]
-        var recvDataBuffer = Buffer.alloc(1024, 0);
+		// BYTE RecvData[1024]
+		const recvDataBuffer = Buffer.alloc(1024, 0);
 
-        // DWORD *RecvDataLen
-        var recvDataLenDWordPtr = ref.alloc(types.dword, 0);
+		// DWORD *RecvDataLen
+		const recvDataLenDWordPtr = ref.alloc(types.dword, 0);
 
-        var ret = libModule.MT188_RFA_APDU(sendDataBuffer, sendData.length / 2, recvDataBuffer, recvDataLenDWordPtr);
+		const ret = libModule.MT188_RFA_APDU(sendDataBuffer, sendData.length / 2, recvDataBuffer, recvDataLenDWordPtr);
 
-        if(ret != 0) {
-            reject(error(ret));
-        } else {
-            var len = ref.deref(recvDataLenDWordPtr) * 2;
-            var recvData = recvDataBuffer.toString('hex').substr(0, len);
-            resolve(recvData);
-        }
-    });
-}
+		if(ret !== 0)
+			return reject(error(ret));
+
+		const len = ref.deref(recvDataLenDWordPtr) * 2;
+		const recvData = recvDataBuffer.toString('hex').substr(0, len);
+		resolve(recvData);
+	});
+};
 
 /**
  * RFA card power off
  */
-exports.RFAPowerOff = function () {
-    return new Promise(function(resolve, reject) {
-        var ret = libModule.MT188_RFA_PowerOff();
+exports.RFAPowerOff = function() {
+	return new Promise((resolve, reject) => {
+		const ret = libModule.MT188_RFA_PowerOff();
 
-        if(ret != 0) {
-            reject(error(ret));
-        } else {
-            resolve();
-        }
-    });
-}
+		if(ret !== 0)
+			return reject(error(ret));
+
+		resolve();
+	});
+};
--- types.js
+++ types.js
@@ -1,7 +1,10 @@
+'use strict';
+
 /**
  * Win32 types definition for libModule.
  * Types, pointers, and references in node. More info http://tootallnate.github.io/ref/
  */
-var ref = require('ref');
+/* eslint-disable-next-line */
+const ref = require('ref');
 
 exports.dword = ref.types.ulong;
